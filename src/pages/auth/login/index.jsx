8a856a6833b24cfdb3d28797b9e993ae081b57d5
fdhhhdjd <nguyentientai10@gmail.com>
2024-10-22 15:07:03 +0700

    feat: add #2 code login done

--- src/pages/auth/login/index.jsx
+++ src/pages/auth/login/index.jsx
@@ -1,16 +1,86 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+import eye from '@/assets/images/auth/Eye.svg';
+import eyeOff from '@/assets/images/auth/EyeOff.svg';
 import Rectangle from '@/components/NavItemSections/Rectangle';
 import Footer from '@/layouts/footer';
 import Navigation from '@/layouts/navigation';
 
 const Login = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <React.Fragment>
       {/* 1. Nav */}
       <Navigation />
       {/* 2. Login */}
-      <h1>Login</h1>
+      <div className='w-full h-full flex justify-center mt-20 mb-20'>
+        <div className='w-full max-w-md px-6 pt-6 pb-8 bg-white rounded-lg shadow border border-[#f2f2f2] flex-col justify-start items-center gap-5 inline-flex'>
+          <div className='text-[#191919] text-[32px] font-semibold font-poppins leading-[38.40px]'>
+            Sign In
+          </div>
+          <div className='flex-col justify-start items-start gap-4 flex w-full'>
+            <div className='w-full flex-col justify-start items-start gap-3 flex'>
+              <div className='w-full px-4 py-3.5 bg-white rounded-md border border-[#e6e6e6] flex justify-start'>
+                <input
+                  type='email'
+                  placeholder='Email'
+                  className='w-full text-black text-base font-normal font-poppins leading-tight focus:outline-none border-none'
+                />
+              </div>
+              <div className='w-full px-4 py-3.5 bg-white rounded-md border border-[#e6e6e6] flex justify-between gap-2'>
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder='Password'
+                  className='w-full text-black text-base font-normal font-poppins leading-tight focus:outline-none border-none'
+                />
+                <div
+                  className='w-5 h-5 cursor-pointer'
+                  onClick={togglePasswordVisibility}
+                >
+                  <img
+                    src={showPassword ? eyeOff : eye}
+                    alt='eye'
+                    className='w-full h-full'
+                  />
+                </div>
+              </div>
+            </div>
+            <div className='w-full  flex justify-between'>
+              <div className='justify-start items-start gap-1.5 flex'>
+                <div className='w-5 h-5'>
+                  <input type='checkbox' className='h-5 w-5 cursor-pointer' />
+                </div>
+                <div className='text-[#666666] text-sm font-normal font-poppins leading-[21px]'>
+                  Remember me
+                </div>
+              </div>
+              <div className='text-[#666666] text-sm font-normal font-poppins leading-[21px]'>
+                Forget Password
+              </div>
+            </div>
+          </div>
+          <button className='w-full  px-8 py-3.5 bg-[#00b206] rounded-[43px] text-white text-sm font-semibold font-poppins leading-[16.80px]'>
+            Login
+          </button>
+          <div className='pt-1 flex gap-2'>
+            <div className='text-[#666666] text-sm font-normal font-poppins leading-[21px]'>
+              Don’t have account?
+            </div>
+            <Link
+              to='/auth/register'
+              className='text-[#191919] text-sm font-medium font-poppins leading-[21px]'
+            >
+              Register
+            </Link>
+          </div>
+        </div>
+      </div>
       {/* 3.Rectangle */}
       <Rectangle />
       {/* 4.Footer */}
